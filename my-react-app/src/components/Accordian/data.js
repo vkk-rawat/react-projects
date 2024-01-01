15c55e61471266a616a7fcf924af88579cb4a630
NoObXxProgrammer <vickyrawat5998@gmail.com>
2024-01-01 11:29:01 +0530

    no

--- my-react-app/src/components/Accordian/data.js
+++ my-react-app/src/components/Accordian/data.js
@@ -1,27 +1,27 @@
 {
-    "accordionItems": [
-      {
-        "id": 1,
-        "title": "Section 1",
-        "content": "This is the content for Section 1."
-      },
-      {
-        "id": 2,
-        "title": "Section 2",
-        "content": "This is the content for Section 2."
-      },
-      {
-        "id": 3,
-        "title": "Section 3",
-        "content": "This is the content for Section 3."
-      },
-      {
-        "id": 4,
-        "title": "Section 4",
-        "content": "This is the content for Section 4."
-      }
-    ]
-  }
-  
+  "accordionItems": [
+    {
+      "id": 1,
+      "title": "Section 1",
+      "content": "This is the content for Section 1."
+    },
+    {
+      "id": 2,
+      "title": "Section 2",
+      "content": "This is the content for Section 2."
+    },
+    {
+      "id": 3,
+      "title": "Section 3",
+      "content": "This is the content for Section 3."
+    },
+    {
+      "id": 4,
+      "title": "Section 4",
+      "content": "This is the content for Section 4."
+    }
+  ]
+}
+
 
 export default DataTransfer;
